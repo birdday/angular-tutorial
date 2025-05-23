b7db0182547b8b5cf47228aeed774990066f244c
Brian Day <22bday@gmail.com>
2025-05-23 15:17:31 -0400

    App Module Variation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  standalone: true, // Default in Angular 19+
+  // standalone: true, // Default in Angular 19+
   // Alternative is modular components (older style).
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,15 +1,11 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { TaskComponent } from './task/task.component';
-import { NewTaskComponent } from './new-task/new-task.component';
+import { Component, Input } from '@angular/core';
 
-import { type Task } from './task/task.model';
-import { type NewTask } from './new-task/new-task.model';
 import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
-  standalone: true,
-  imports: [TaskComponent, NewTaskComponent],
+  // standalone: true,
+  // imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
